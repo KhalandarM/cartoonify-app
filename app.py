aa7d929dc31049af713a1b5ea23739f99b2c56c4
Noorysaba22 <sabanoory422@gmail.com>
2025-04-22 12:54:19 +0530

    fix: set host and port for Render

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ def serve_static(filename):
 
 if __name__ == '__main__':
     import os
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))  # Render will set this automatically
+    app.run(host='0.0.0.0', port=port, debug=True)
